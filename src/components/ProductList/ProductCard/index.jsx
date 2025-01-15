93ec1ee7f1697b1f6f3ebfc22fc638c24c8d5cd6
vnshas <163927528+vnshas@users.noreply.github.com>
2025-01-15 13:59:38 -0300

    finalizado

--- src/components/ProductList/ProductCard/index.jsx
+++ src/components/ProductList/ProductCard/index.jsx
@@ -2,7 +2,7 @@ import styles from "./style.module.scss"
 
 export const ProductCard = ({ product, addToCart }) => {
     return (
-      <li key={product.id}>
+      <li key={product.id} tabIndex={0}>
         <div className={styles.CardImage}><img src={product.img} alt={product.name} /></div>
         <div className={styles.Category}>
           <label htmlFor="item-1"></label>
